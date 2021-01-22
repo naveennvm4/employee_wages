2792c671a58ba0dd0e60e307c6941da6e7ac3a45
naveennvm4 <naveennvm4@gmail.com>
2021-01-22 19:41:04 +0530

    Using case statement

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,14 +1,17 @@
 isParttime=1;
 isFulltime=2;
-randomCheck=$((RANDOM%3));
-if [ $isPFulltime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isParttime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
+empRatePerhr=20;
+empCheck=$((RANDOM%3));
+case $rmpCheck in
+	$isFulltime)
+		empHrs=8
+		;;
+	$isParttime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
 salary=$(($empHrs*$empRatePerHr));
 
