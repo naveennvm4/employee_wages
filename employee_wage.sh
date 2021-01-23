13ff39494f01284f44a6fc9911d02b36b85af2e5
naveennvm4 <naveennvm4@gmail.com>
2021-01-23 08:19:10 +0530

    Calculating monthly salary

--- employee_wage.sh
+++ employee_wage.sh
@@ -2,15 +2,21 @@ isParttime=1;
 isFulltime=2;
 empRatePerhr=20;
 workingdays=20;
-randomCheck=$((RANDOM%3));
-if [ $isFulltime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isParttime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-salary=$(($empHrs*$empRatePerHr*$workingdays));
-echo "monthly salary=$salary"
+totalSalary=0;
+for (( day=1; day<=$workingdays; day++ ))
+do
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+		$isFulltime)
+			empHrs=8
+			;;
+		$isParttime)
+			empHrs=4
+			;;
+		*)
+		empHrs=0 
+			;;
+esac
+salary=$(($empHrs*$empRatePerHr));
+totalSalary=$(($totalSalary+$salary));
+done
